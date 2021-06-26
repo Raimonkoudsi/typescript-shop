f20226b8cbf50e19f83ea311ad670622785fbc86
Raimonkoudsi <ramonkoudsi@gmail.com>
2021-06-25 16:01:17 -0930

    cards & cardItems

---
+++ react-shopping-cart/src/Cart/Cart.tsx
@@ -0,0 +1,33 @@
+import CartItem from '../CartItem/CartItem';
+
+//estilos
+import { Wrapper } from './Cart.styles';
+
+//tipos
+import { CartItemType } from '../App';
+
+
+type Props = {
+    cartItems: CartItemType[];
+    addToCart: (clickedItem: CartItemType) => void;
+    removeFromCart: (id: number) => void;
+}
+
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+    return (
+        <Wrapper>
+            <h2>Your Shopping Cart</h2>
+            {cartItems.length === 0 ? <p>No Items in Cart.</p> : null}
+            {cartItems.map(item => (
+                <CartItem 
+                    key={item.id}
+                    item={item}
+                    addToCart={addToCart}
+                    removeFromCart={removeFromCart}
+                />
+            ))}
+        </Wrapper>
+    );
+};
+
+export default Cart;
